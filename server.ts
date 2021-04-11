57de4590e3c79c7c9c81cc213c9c52d04bef68ba
Leandro Cortiz <49104364+leandrozitroc@users.noreply.github.com>
2021-04-11 17:46:58 -0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,16 +1,6 @@
 import lib from 'pipedrive'
 import Appcontroller from "./app"
 
-class Server{
-    app = Appcontroller.express;
-    lib = lib.Configuration.apiToken = `${process.env.PIPEDRIVE_API_KET}`;
-    constructor(){
-        this.app = app
-        this.lib = lib
-
-    }
-}
-
 const app = Appcontroller.express
 
 lib.Configuration.apiToken = `${process.env.PIPEDRIVE_API_KET}`;
@@ -19,4 +9,4 @@ app.listen(1800)
 app.listen(3000)
 
 
-export default new Server();
+
